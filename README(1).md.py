8260673e7ba4de3c849b4e1e1adf3070a04c3b9f
WiseRed99 <84101106+WiseRed99@users.noreply.github.com>
2023-05-11 09:57:01 -0700

    Update README(1).md.py

--- README(1).md.py
+++ README(1).md.py
@@ -16,14 +16,8 @@ PyCharm: https://www.jetbrains.com/help/pycharm/installation-guide.html#toolbox
 Once installed you can run the program with the following command
 
 ```
-python cna_demo.py
-```
-
-Add any additional ways to run the program below
-
-```
-python cna_demo.py test.txt
+python README(1).md.py
 ```
 
 ## Thanks
-Provide thank yous and attributions here. If someone helped you, you looked at another repository, or another article, provide it here.
+Thank you to Dr. Zak for providing the demos needed to do this homework, and for the PyCharm app to be able to run this code.

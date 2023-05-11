44c23e6ed085234d9f2fe34ea04cee24f97b3e59
WiseRed99 <wcgrainger@student.rtc.edu>
2023-05-11 14:17:16 -0700

    Create New Python Code.py
    Adding information about my school homework.

---
+++ New Python Code.py
@@ -0,0 +1,27 @@
+New Python Code.
+
+This code provides a way to look at a list in python charm and see if the word(s) exist in that list. If it meets a certain criteria (being true), than it will be printed. However, if it does not meet the criteria (being false), than it will be not printed and will continue on to the next object/string in the list.
+
+## Getting Started
+
+These instructions will run this code on your local machine for experimenting in the python charm app.
+
+### Prerequisites
+
+the Python code requires a PyCharm to run. The commands below will download a Python app.
+
+PyCharm: https://www.jetbrains.com/pycharm/download/#section=windows
+
+## Running
+```
+click on the download button under the community one, and follow the instructionson. 
+```
+
+Once installed you can run the program with the following command
+
+```
+python README(1).md.py
+```
+
+## Thanks
+Thank yous to Dr. Zak for providing the demos needed to do this homework, for GitHub on how to provide an excellent writing README.md files so effectively, and for the PyCharm app to be able to run this code.

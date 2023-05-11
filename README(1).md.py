7082bd8235726a5bb45e2967ce8be326ad1fcb41
WiseRed99 <wcgrainger@student.rtc.edu>
2023-05-10 19:46:27 -0700

    Updated README.md
    Added additional information to the readme file.

--- README(1).md.py
+++ README(1).md.py
@@ -1,20 +1,15 @@
 New Python Code.
 
-One Paragraph of project description goes here.
+This code provides a way to look at a list in python charm and see if the word(s) exist in that list. If it meets a certain criteria (being true), than it will be printed. However, if it does not meet the criteria (being false), than it will be not printed and will continue on to the next object/string in the list.
 
 ## Getting Started
 
-These instructions will [do something] on your local machine for [development/experimentation/demo].
+These instructions will run this code on your local machine for experimenting in the python charm app.
 
 ### Prerequisites
 
-[Project] requires [software and version] to run, with [additional packages, libaries, or mods]. The commands below will [upgrade OS and install the prerequisites, or do something else]
-
-```
-sudo apt update
-sudo apt upgrade
-sudo apt install package1 package2
-```
+the Python code requires a PyCharm to run. The commands below will download a Python app.
+PyCharm: https://www.jetbrains.com/help/pycharm/installation-guide.html#toolbox
 
 ## Running
 Once installed you can run the program with the following command
